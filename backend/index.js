02a0c1efe246dd2c51ef228c6767b34cd4c553ce
Shraddha Dhote <soniyadhote8@gmail.com>
2026-07-26 10:59:16 -0700

    fix: updated cors options and added preflight options handler

--- backend/index.js
+++ backend/index.js
@@ -23,9 +23,11 @@ app.use(cookieParser());
 const corsOptions = {
     origin: ["http://localhost:5173", "https://job-portal-frontend-eight-iota.vercel.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 
 }
-app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 
 const PORT = process.env.PORT || 5001;
