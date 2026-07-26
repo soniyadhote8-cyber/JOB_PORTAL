16f30efa704f6696b490e1cf5714b545adacf5f8
Shraddha Dhote <soniyadhote8@gmail.com>
2026-07-25 23:53:54 -0700

    fix api links

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT = "http://localhost:5001/api/user"
-export const JOB_API_ENDPOINT = "http://localhost:5001/api/job"
-export const APPLICATION_API_ENDPOINT = "http://localhost:5001/api/application"
-export const COMPANY_API_ENDPOINT = "http://localhost:5001/api/company"
+export const USER_API_ENDPOINT = "https://vercel.app"
+export const JOB_API_ENDPOINT = "https://vercel.app"
+export const APPLICATION_API_ENDPOINT = "https://vercel.app"
+export const COMPANY_API_ENDPOINT = "https://vercel.app"
